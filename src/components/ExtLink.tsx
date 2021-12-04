24595bf512d6d5a2b1d1e894eb4898970fe201a6
sim <sim@terra.money>
2021-12-04 20:34:53 +0900

    Validate external link

--- src/components/ExtLink.tsx
+++ src/components/ExtLink.tsx
@@ -1,10 +1,18 @@
 import { AnchorHTMLAttributes } from "react"
 
 export type Props = AnchorHTMLAttributes<HTMLAnchorElement>
-const ExtLink = ({ children, ...attrs }: Props) => (
-  <a {...attrs} target="_blank" rel="noopener noreferrer">
-    {children}
-  </a>
-)
+const ExtLink = ({ children, ...attrs }: Props) => {
+  if (!attrs.href) return null
+
+  /* validate */
+  const url = new URL(attrs.href)
+  if (!(url.protocol === "http:" || url.protocol === "https:")) return null
+
+  return (
+    <a {...attrs} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
+  )
+}
 
 export default ExtLink
