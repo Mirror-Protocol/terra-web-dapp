c8e8f036cbe4c3eda0c77955ca674a6856e737a2
sim <sim@terra.money>
2022-02-07 20:32:02 +0900

    Add LunaX collateral for testnet

--- src/data/external/external.ts
+++ src/data/external/external.ts
@@ -1,18 +1,35 @@
 import { atom, selector } from "recoil"
 import { useStore, useStoreLoadable } from "../utils/loadable"
 import * as anchor from "./anchor"
+import * as lunax from "./lunax"
 
 export const whitelistExternalQuery = selector({
   key: "whitelistExternal",
   get: ({ get }) => {
     const anchorProtocol = get(anchor.anchorProtocolQuery)
-    return { ...anchorProtocol.assets }
+    const LunaXAsset = get(lunax.assetQuery)
+
+    return Object.assign(
+      {},
+      anchorProtocol.assets,
+      LunaXAsset && { [LunaXAsset.token]: LunaXAsset }
+    )
   },
 })
 
 export const externalPricesQuery = selector({
   key: "externalPrices",
-  get: ({ get }) => ({ ...get(anchor.anchorPricesQuery) }),
+  get: ({ get }) => {
+    const anchorPrices = get(anchor.anchorPricesQuery)
+    const LunaXAsset = get(lunax.assetQuery)
+    const LunaXPrice = get(lunax.priceQuery)
+
+    return Object.assign(
+      {},
+      anchorPrices,
+      LunaXAsset && { [LunaXAsset.token]: LunaXPrice }
+    )
+  },
 })
 
 const externalPricesState = atom<Dictionary>({
@@ -22,7 +39,17 @@ const externalPricesState = atom<Dictionary>({
 
 export const externalBalancesQuery = selector({
   key: "externalBalances",
-  get: ({ get }) => ({ ...get(anchor.anchorBalancesQuery) }),
+  get: ({ get }) => {
+    const anchorBalances = get(anchor.anchorBalancesQuery)
+    const LunaXAsset = get(lunax.assetQuery)
+    const LunaXBalance = get(lunax.balanceQuery)
+
+    return Object.assign(
+      {},
+      anchorBalances,
+      LunaXAsset && { [LunaXAsset.token]: LunaXBalance }
+    )
+  },
 })
 
 const externalBalancesState = atom<Dictionary>({
---
+++ src/data/external/lunax.ts
@@ -0,0 +1,59 @@
+import { selector } from "recoil"
+import { networkNameState } from "../network"
+import { getContractQueryQuery } from "../utils/query"
+import { protocolQuery } from "../contract/protocol"
+import { addressState } from "../wallet"
+
+const testnet: ListedItemExternal = {
+  symbol: "LunaX",
+  token: "terra1v0ypm2yc96alhn634pnwt4q4px482ukfqk02hx",
+  icon: "https://raw.githubusercontent.com/stader-labs/assets/main/terra/LunaX_1.png",
+  status: "LISTED",
+}
+
+export const assetQuery = selector({
+  key: "LunaX.asset",
+  get: ({ get }) => {
+    const networkName = get(networkNameState)
+    return { testnet }[networkName]
+  },
+})
+
+export const priceQuery = selector<string>({
+  key: "LunaX.price",
+  get: async ({ get }) => {
+    const asset = get(assetQuery)
+    const { contracts } = get(protocolQuery)
+    const getQuery = get(getContractQueryQuery)
+
+    if (!asset) throw new Error("LunaX is not defined")
+
+    const result = await getQuery<{ rate: string }>(
+      {
+        contract: contracts["collateralOracle"],
+        msg: { collateral_price: { asset: asset.token } },
+      },
+      "collateralOraclePrice" + asset.symbol
+    )
+
+    return result?.rate ?? "0"
+  },
+})
+
+export const balanceQuery = selector<string>({
+  key: "LunaX.balance",
+  get: async ({ get }) => {
+    const address = get(addressState)
+    const asset = get(assetQuery)
+    const getQuery = get(getContractQueryQuery)
+
+    if (!asset) throw new Error("LunaX is not defined")
+
+    const result = await getQuery<Balance>(
+      { contract: asset.token, msg: { balance: { address } } },
+      "balance" + asset.symbol
+    )
+
+    return result?.balance ?? "0"
+  },
+})
