f2ce16c544c74402ef73f3710c2ec511b845fb5c
AnonymousQwer <anonymous_qwer@outlook.com>
2022-03-15 10:42:31 +0700

    Add useTnsName hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useNetwork } from "./useNetwork"
 export { default as useAddress } from "./useAddress"
 export { default as useTerraAssets } from "./useTerraAssets"
+export { default as useTnsName } from "./useTnsName"
