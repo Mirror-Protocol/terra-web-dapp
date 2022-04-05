1df42cc0efa939773b17718bfa7e905d3eeab616
sim <sim@terra.money>
2022-04-05 20:45:59 +0800

    Memoize the date on the function to determine market closed

--- src/forms/modules/useGetIsMarketClosed.tsx
+++ src/forms/modules/useGetIsMarketClosed.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import { useQuery } from "react-query"
 import { useLCDClient } from "@terra-money/wallet-provider"
 import { useProtocol } from "../../data/contract/protocol"
@@ -15,12 +16,17 @@ const useGetIsMarketClosed = (token: string) => {
     { enabled: token !== "uusd" }
   )
 
+  const now = useMemo(() => {
+    if (!data) return 0
+    return new Date().valueOf()
+  }, [data])
+
   // false === market is opened
   // true === market is closed
 
   if (data) {
     const { last_updated } = data
-    const sec = Math.floor(new Date().valueOf() / 1000) - last_updated
+    const sec = Math.floor(now / 1000) - last_updated
     return sec > 60 || getIsDelisted(token)
   }
 
