9e77b4695e372a9ba2b23967f7c9b0821052a7c2
James0126 <james@terra.money>
2022-05-10 10:13:41 +0900

    Handle vote button issue

--- src/pages/Poll/VoteLink.tsx
+++ src/pages/Poll/VoteLink.tsx
@@ -4,18 +4,20 @@ import { Poll } from "../../data/gov/poll"
 import { Submit } from "../../components/Button"
 import LinkButton from "../../components/LinkButton"
 
-const VoteLink = ({ id, end_time }: Poll) => {
+const VoteLink = ({ id, end_time, status }: Poll) => {
   const { url } = useRouteMatch()
   const params = useParams<{ id: string }>()
   const getVoted = useGetVoted()
   const voted = getVoted(id)
 
+  const inactive = ["executed", "passed"].includes(status)
+
   const end = end_time * 1000 < Date.now()
 
   return params.id && !end ? (
     <Submit>
-      <LinkButton to={url + "/vote"} disabled={voted}>
-        {voted ? "Voted" : "Vote"}
+      <LinkButton to={url + "/vote"} disabled={voted || inactive}>
+        {voted ? "Voted" : inactive ? "Inactive" : "Vote"}
       </LinkButton>
     </Submit>
   ) : null
